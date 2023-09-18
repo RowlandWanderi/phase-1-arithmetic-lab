4e35a6af91821fda128ef4be890c48b8418f6682
Rowland <rowland.wanderi@student.moringaschool.com>
2023-09-18 14:13:59 +0300

    complete assignment

--- index.js
+++ index.js
@@ -1,2 +1,10 @@
 
-// Write your code here
+// Write your code 
+const num1 = 2;
+const num2 = 31;
+const num3 = 22;
+const num4 = 6;
+let multiply = num1 * num2;
+let random = Math.floor(Math.random() * 100) + 1;
+let mod = num3 % num4;
+let max = Math.max(5, 20);
